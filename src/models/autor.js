4d4de3934dcfa8fdf2937552ca8cf70b5966793c
Gustavo Sabino <gu.sabino@hotmail.com>
2026-05-26 20:17:06 -0300

    feat: :sparkles: add:  métodos para listar livros por autor

--- src/models/autor.js
+++ src/models/autor.js
@@ -63,6 +63,10 @@ class Autor {
     const resultado = await this.criar();
     return resultado;
   }
+
+  static async pegaLicrosPorAutor(autorId) {
+    return db('livros').where({ autor_id: autorId });
+  }
 }
 
 export default Autor;
