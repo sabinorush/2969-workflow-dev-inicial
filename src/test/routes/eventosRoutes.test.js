aa67ec08979a1287fa32308dba76cf4a16541401
Gustavo Sabino <gu.sabino@hotmail.com>
2026-06-03 19:11:12 -0300

    feat: :sparkles: add: feature flash unleash para eventos

--- src/test/routes/eventosRoutes.test.js
+++ src/test/routes/eventosRoutes.test.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-unused-expressions */
+
 import { after } from 'mocha';
+
 import chai from 'chai';
+
 import chaiHttp from 'chai-http';
+
+import sinon from 'sinon';
+
 import app from '../../app.js';
+
 import db from '../../db/dbconfig.js';
 
+import EventosController from '../../controllers/eventosController.js';
+
 chai.use(chaiHttp);
+
 const { expect } = chai;
+
+let stub;
 
 after(async () => {
   await db.destroy();
@@ -14,26 +26,43 @@ after(async () => {
 
 describe('GET em /eventos', () => {
   it('Deve retornar uma lista de eventos', (done) => {
-    process.env.EVENTO_FLAG = 'true';
+    stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(true);
+
     chai.request(app)
+
       .get('/eventos')
+
       .set('Accept', 'application/json')
+
       .end((err, res) => {
         expect(res.status).to.equal(200);
+
         expect(res.body).to.be.an('array');
+
         expect(res.body[0]).to.have.property('id');
+
         expect(res.body[0]).to.have.property('nome');
+
         expect(res.body[0]).to.have.property('descricao');
+
         done();
       });
   });
+
   it('Deve retornar erro 404', (done) => {
-    process.env.EVENTO_FLAG = 'false';
+    stub.restore();
+
+    stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(false);
+
     chai.request(app)
+
       .get('/eventos')
+
       .set('Accept', 'application/json')
+
       .end((err, res) => {
         expect(res.status).to.equal(404);
+
         done();
       });
   });
